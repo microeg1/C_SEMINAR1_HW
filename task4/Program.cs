399030ae45e46e8929188ebc7d9235c4e5409308
Egor <pro100egar@gmail.com>
2022-08-19 23:01:19 +0700

    task4

---
+++ task4/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите число.");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+
+while(count < number1)
+{
+    if(count % 2 == 0)
+    {
+        Console.Write(count);
+        Console.Write(", ");
+        count++;
+    }
+    else
+    {
+        count++;
+    }
+
+}
