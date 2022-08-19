b12e80c109b1107e75ce56b02a1f3667a041566e
Egor <pro100egar@gmail.com>
2022-08-19 23:00:15 +0700

    task1

---
+++ task1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите два числа.");
+Double number1 = Convert.ToDouble(Console.ReadLine());
+Double number2 = Convert.ToDouble(Console.ReadLine());
+if(number1 > number2)
+{
+    Console.Write(number1);
+    Console.WriteLine(" - большее число.");
+}
+else
+{
+    Console.Write(number2);
+    Console.WriteLine(" - большее число.");
+}
