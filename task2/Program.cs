64714ef2d3c788500e4d4075982cdbf6da04817e
Egor <pro100egar@gmail.com>
2022-08-19 23:00:50 +0700

    task2

---
+++ task2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите три числа.");
+Double number1 = Convert.ToDouble(Console.ReadLine());
+Double number2 = Convert.ToDouble(Console.ReadLine());
+Double number3 = Convert.ToDouble(Console.ReadLine());
+
+double max = 0;
+
+if(number1 > max) max = number1;
+if(number2 > max) max = number2;
+if(number3 > max) max = number3;
+
+Console.Write(max);
+Console.WriteLine(" - самое большое число.");
